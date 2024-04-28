4ce317c987f2715779a81b7684d8e0f80e272914
b-smyers <bs602422@ohio.edu>
2024-04-28 14:33:14 -0400

    Mostly working full flag

--- src/wiki.py
+++ src/wiki.py
@@ -8,7 +8,10 @@ def clean_references(text):
     # Use regular expressions to remove references, e.g., [1], [2], etc.
     return re.sub(r'\[\d+\]', '', text)
 
-def search_wikipedia(query):
+def clean_edits(text):
+    return re.sub(r'\[edit\]', '', text)
+
+def search_wikipedia(query, args):
     query = query.replace(' ', '_')
     response = requests.get(f"https://en.wikipedia.org/wiki/{query}")
 
@@ -21,26 +24,34 @@ def search_wikipedia(query):
     
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Extract and display the info box if present
-    infobox = soup.find('table', class_='infobox')
-    if infobox:
-        print(Style.BRIGHT + clean_references(infobox.get_text(separator='\n', strip=True)) + Style.RESET_ALL + '\n')
+    content_div = soup.find(class_='mw-content-ltr')
+    content = content_div.find_all(['h2','h3','p', 'li'], recursive=True)
+    print(Style.BRIGHT + Fore.CYAN + "========== Summary ==========" + Fore.RESET + Style.RESET_ALL)
 
-    # Extract the first few paragraphs
-    content = soup.find(id='mw-content-text').find_all('p')
+    for child in content:
+        text = clean_edits(clean_references(child.get_text().strip()))
+        if not text:
+            continue
+        
+        if child.name == 'h2':
+            if text == 'Other Uses' or text == 'See also' or text == 'References':
+                break
+            if not args.full:
+                break
+            print(Style.BRIGHT + Fore.CYAN + '========== ' + text + ' ==========' + Fore.RESET + Style.RESET_ALL)
+        elif child.name == 'h3':
+            print(Style.BRIGHT + Fore.MAGENTA + '----- ' + text + ' -----' + Fore.RESET + Style.RESET_ALL)
+        elif child.name == 'li':
+            print(' * ' + text)
+        elif child.name == 'p':
+            print(text)
+
     search_url = response.url
-    print(Style.RESET_ALL + "Summary:")
-    for i, p in enumerate(content):
-        if i >= 3:  # Limit to first 3 paragraphs
-            break
-        cleaned_text = clean_references(p.get_text().strip())
-        if cleaned_text:
-            print(cleaned_text)
 
     print(Fore.BLUE + "Read more:", search_url)
 
-def search_top_results(query):
-    response = requests.get(f"https://en.wikipedia.org/w/index.php?fulltext=1&search={query}&title=Special%3ASearch&ns0=1")
+def search_top_results(args):
+    response = requests.get(f"https://en.wikipedia.org/w/index.php?fulltext=1&search={args.query}&title=Special%3ASearch&ns0=1")
 
     if response.status_code != 200:
         print(Fore.RED + 'There is a problem with fetching data. Please try again!')
@@ -49,12 +60,12 @@ def search_top_results(query):
     soup = BeautifulSoup(response.content, 'html.parser')
     search_results_ul = soup.find('ul', class_='mw-search-results')
     if not search_results_ul:
-        print(f'{Fore.RED}No results could be found for "{query}".')
+        print(f'{Fore.RED}No results could be found for "{args.query}".')
         return
 
     top_results = search_results_ul.find_all('li', class_='mw-search-result', limit=10)
 
-    print(f'Here are the results for: \"{query}\"')
+    print(f'Here are the results for: \"{args.query}\"')
     for i, item in enumerate(top_results):
         title = item.find('a').get('title').strip()
         desc = item.find('div', class_='searchresult').get_text().strip()
@@ -66,7 +77,7 @@ def search_top_results(query):
         if page.lower() == 'q':
             return  # User wants to quit
         elif page.isdigit() and 1 <= int(page) <= len(top_results):
-            search_wikipedia(top_results[int(page)-1].find('a').get('title').strip())
+            search_wikipedia(top_results[int(page)-1].find('a').get('title').strip(), args)
             break
         else:
             print(Fore.RED + "Invalid input, please try again.")
@@ -74,8 +85,9 @@ def search_top_results(query):
 def main():
     parser = argparse.ArgumentParser(description='Search for Wikipedia pages.')
     parser.add_argument('query', type=str, help='The Wikipedia page to search for')
+    parser.add_argument('-f', '--full', action='store_true', help='Returns full text from Wikipedia article content')
     args = parser.parse_args()
-    search_top_results(args.query)
+    search_top_results(args)
 
 if __name__ == "__main__":
     main()
